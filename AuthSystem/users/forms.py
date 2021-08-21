318d3049fc4a7a4121104f17b35b601f44730523
Md. Al-Imran <asq24i@gmail.com>
2021-08-21 20:08:56 +0600

    login works

---
+++ AuthSystem/users/forms.py
@@ -0,0 +1,42 @@
+from .models import UserProfile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget = forms.EmailInput(attrs = {'class': 'form-control', 'placeholder' : 'email'}))
+    first_name = forms.CharField(max_length=50, widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder' : 'First Name'}))
+    last_name = forms.CharField(max_length=50,  widget = forms.TextInput(attrs = {'class': 'form-control', 'placeholder' : 'Last Name'}))
+    
+    class Meta:
+        model = UserProfile
+        fields = ('email','first_name','last_name','phone','password1','password2')
+
+       
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+class UserLoginForm(forms.ModelForm):
+    password = forms.CharField(label="password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = UserProfile
+        fields = ('email','password')
+
+    def clean(self):
+        if self.is_valid():
+            email=self.cleaned_data['email']
+            password = self.cleaned_data['password']
+
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError("Invalid Email or Password")
+
+
+
--- AuthSystem/users/views.py
+++ AuthSystem/users/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
+from .models import UserProfile
+from users.forms import SignUpForm, UserLoginForm 
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+#Create your views here.
+@login_required(login_url='login/')
+
+def home(request):
+    return render(request, "registration/user_information.html")
+
+
+
+def register(request):
+    context = {}
+    if request.POST:
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+        context['register_form']=form
+
+    else:
+        form=SignUpForm()
+        context['register_form']=form
+    return render(request, 'registration/register.html', context)
+
+
+def login_view(request):
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'login_form': form,
+    }
+    return render(request, "registration/login.html", context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login/')
+
+
+    
