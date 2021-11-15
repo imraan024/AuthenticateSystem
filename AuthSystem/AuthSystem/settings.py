a20b90dde60b09ce78977a2b6ff350494b0df901
imraan024 <asq24i@gmail.com>
2021-11-16 02:23:09 +0600

    bug fixed

--- AuthSystem/AuthSystem/settings.py
+++ AuthSystem/AuthSystem/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alimraan24@gmail.com'
-EMAIL_HOST_PASSWORD = 'Your Password'
+EMAIL_HOST_PASSWORD = 'your password'
